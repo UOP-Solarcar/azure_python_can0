f81a919fccdbbf924ff576a97db122900009d35b
pranav-jay26 <117538632+pranav-jay26@users.noreply.github.com>
2023-09-19 23:43:33 -0700

    Add files via upload
    parses CAN messages and sends to IoT Hub

---
+++ canmsg-parse.py
@@ -0,0 +1,41 @@
+import time
+from azure.iot.device import IoTHubDeviceClient, Message
+import can
+
+CONNECTION_STRING = "HostName=uopsolarcartest001.azure-devices.net;DeviceId=raspberry-pi-solarcar;SharedAccessKey=t0/PkF0TugKeJNCKvBWd7YKOgrpPr0hOM7ZfNy1rA0A=
+"
+DEVICE_ID = "t0/PkF0TugKeJNCKvBWd7YKOgrpPr0hOM7ZfNy1rA0A="
+
+def send_sensor_data(data):
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+    try:
+        client.connect()
+        message = Message(data)
+        client.send_message(message)
+        print(f"Sent: {data}")
+    except Exception as e:
+        print(f"Error sending data: {str(e)}")
+    finally:
+        client.disconnect()
+
+def read_data_from_can_bus():
+    bus = can.interface.Bus(channel='can0', bustype='socketcan')
+
+    try:
+        message = bus.recv(timeout=1)  # Adjust the timeout as needed
+        if message is not None:
+
+            sensor_data = message.data  # WIP, will change based on sensor data format
+            return sensor_data
+    except can.CanError:
+        print("Error reading from CAN bus")
+
+    return None
+
+while True:
+    # Read data from Arduino over CAN bus
+    arduino_data = read_data_from_can_bus()
+    if arduino_data is not None:
+        send_sensor_data(arduino_data)
+    time.sleep(5)  # Send data every 5 seconds
