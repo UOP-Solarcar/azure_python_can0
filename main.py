85bdcf69fc58fc1416aff8fd222dfc605d045a46
MiguelX413 <lbpgaming27@gmail.com>
2023-09-20 07:48:40 +0000

    Clean canmsg-parse.py and rename to main.py, add black.yml and .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
 import os
 import time
-from azure.iot.device import IoTHubDeviceClient, Message
+
 import can
+from azure.iot.device import IoTHubDeviceClient, Message
 
 CONNECTION_STRING = f"HostName=uopsolarcartest001.azure-devices.net;DeviceId=raspberry-pi-solarcar;SharedAccessKey={os.getenv('sharedaccesskey')}"
-DEVICE_ID = os.getenv('deviceid')
+DEVICE_ID = os.getenv("deviceid")
+
 
 def send_sensor_data(data):
     client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
@@ -15,17 +18,17 @@ def send_sensor_data(data):
         client.send_message(message)
         print(f"Sent: {data}")
     except Exception as e:
-        print(f"Error sending data: {str(e)}")
+        print(f"Error sending data: {e}")
     finally:
         client.disconnect()
 
+
 def read_data_from_can_bus():
-    bus = can.interface.Bus(channel='can0', bustype='socketcan')
+    bus = can.interface.Bus(channel="can0", bustype="socketcan")
 
     try:
         message = bus.recv(timeout=1)  # Adjust the timeout as needed
         if message is not None:
-
             sensor_data = message.data  # WIP, will change based on sensor data format
             return sensor_data
     except can.CanError:
@@ -33,9 +36,15 @@ def read_data_from_can_bus():
 
     return None
 
-while True:
-    # Read data from Arduino over CAN bus
-    arduino_data = read_data_from_can_bus()
-    if arduino_data is not None:
-        send_sensor_data(arduino_data)
-    time.sleep(5)  # Send data every 5 seconds
+
+def main() -> None:
+    while True:
+        # Read data from Arduino over CAN bus
+        arduino_data = read_data_from_can_bus()
+        if arduino_data is not None:
+            send_sensor_data(arduino_data)
+        time.sleep(5)  # Send data every 5 seconds
+
+
+if __name__ == "__main__":
+    main()
